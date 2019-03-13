26590b513c1794f26c0fd8e9ac9ef8f36c3028b7
Anass Daoudi <anass1995daoudi@gmail.com>
2019-03-13 22:51:33 +0000

    Create Main to execute all components examples

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Counter from './state-hook-examples/Counter';
+import Todos from './state-hook-examples/Todos';
+import VisibilityToggling from './effect-hook-examples/VisibilityToggling';
+import VisibilityTogglingWithHoverTimes from './effect-hook-examples/VisibilityTogglingWithHoverTimes';
+import VisibilityTogglingWithHoverTimesV2 from './effect-hook-examples/VisibilityTogglingWithHoverTimesV2';
+import AsyncIncCounterWithHOCExample from './hoc-vs-hooks/AsyncIncCounterWithHOCExample';
+import AsyncIncCounterWithHooksSExample from './hoc-vs-hooks/AsyncIncCounterWithHooksSExample';
+
+const Main = () => (
+    <React.Fragment>
+        <Counter />
+        <hr />
+        <Todos />
+        <hr />
+        <VisibilityToggling />
+        <hr />
+        <VisibilityTogglingWithHoverTimes />
+        <hr />
+        <VisibilityTogglingWithHoverTimesV2 />
+        <hr />
+        <AsyncIncCounterWithHOCExample />
+        <hr />
+        <AsyncIncCounterWithHooksSExample />
+    </React.Fragment>
+);
+
+export default Main;
